f8a43fd53c6ad11182d4a13af5b47c0c1bd218e8
Casey McLaughlin <caseyamcl@gmail.com>
2012-12-03 13:41:17 -0500

    Added ability to parse YML files as YAML

---
+++ src/Configula/Drivers/Yml.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Configula YAML File Driver Alias
+ *
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ * @license MIT
+ * @package Configula
+ */
+
+namespace Configula\Drivers;
+use Configula\DriverInterface;
+
+class Yml extends Yaml implements DriverInterface
+{
+    /* same */
+}
+
+/* EOF: Yml.php */
---
+++ tests/YmlDriverTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Configula YAML Driver Class Unit Test
+ *
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ * @license MIT
+ * @package Configula
+ * @subpackage Unit Tests
+ */
+
+class YmlDriverTest extends YamlDriverTest
+{
+    /* same as parent class */
+}
+
+/* EOF: YmlDriverTest.php */
