a4945df505bfb6821c656993785cf1354992f629
Casey McLaughlin <caseyamcl@gmail.com>
2019-04-10 10:53:09 -0400

    Add license to top of PHP files.

--- tests/Util/ArrayUtilsTest.php
+++ tests/Util/ArrayUtilsTest.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * Configula Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/configula
+ * @version 3.0
+ * @package caseyamcl/configula
+ * @author Casey McLaughlin <caseyamcl@gmail.com>
+ *
+ * For the full copyright and license information, - please view the LICENSE.md
+ * file that was distributed with this source code.
+ *
+ * ------------------------------------------------------------------
+ */
 
 namespace Configula\Util;
 
